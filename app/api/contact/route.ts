272be9416435d3300d406583444a397d4c64207c
Rob <robbworks.dev@gmail.com>
2026-05-26 18:19:40 -0400

    route update

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 import { NextResponse } from "next/server";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
